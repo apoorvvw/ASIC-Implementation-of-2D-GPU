44208d5bafb8b70b3aff9e0fc60c7d6a76924e6c
rastogishubham <rastogis@purdue.edu>
2016-04-19 19:04:12 -0400

    made changed with sizes
    Still need to test. Changed coordinates to be 48 bit, changed vertice num to be 1 bit, 0 means lines, 1 means triangle. Changed total instruction length to be 83 bits

--- decode_block.sv
+++ decode_block.sv
@@ -7,13 +7,13 @@
 // Description: Decode Block for Decoding instructions
 module decode_block
 (
-	input wire fifo_data[100:0],
-	output reg coordinates[63:0],
+	input wire fifo_data[82:0],
+	output reg coordinates[47:0],
 	output reg alpha_val[3:0],
 	output reg texture_code[1:0],
 	output reg color_code[23:0],
 	output reg layer_num[1:0],
-	output reg vertice_num[2:0],
+	output reg vertice_num,
 	output reg inst_type,
 	output reg fill_type
 );
@@ -22,37 +22,32 @@ begin
 	inst_type = fifo_data[0];
 	if(fifo_data[0] == 1'd0) //instruction type is not alpha
 	begin
-		vertice_num = fifo_data[3:1];
-		if(fifo_data[3:1] == 3'd2) //vertice number is 2
+		vertice_num = fifo_data[1];
+		if(fifo_data[1] == 1'd0) //vertice number is 2
 		begin
-			coordinates = {fifo_data[19:4], fifo_data[35:20], '0};
+			coordinates = {fifo_data[17:2], fifo_data[33:18], '0};
 		end
-		else if(fifo_data[3:1] == 3'd3) //vertice number is 3
+		else if(fifo_data[3:1] == 1'd1) //vertice number is 3
 		begin
-			coordinates = {fifo_data[19:4], fifo_data[35:20], fifo_data[51:36], '0};
+			coordinates = fifo_data[17:2], fifo_data[33:18], fifp_data[49:34]};
 		end
-		else
+		layer_num = fifo_data[51:50];
+		if(fifo_data[52] == 1'd0) //fill type is solid color
 		begin
-			coordinates = {fifo_data[19:4], fifo_data[35:20], fifo_data[51:36], fifo_data[67:52]};
-		end
-		layer_num = fifo_data[69:68];
-		if(fifo_data[70] == 1'd0) //fill type is solid color
-		begin
-			fill_type = fifo_data[70];
-			color_code = fifo_data[94:71];
+			fill_type = fifo_data[52];
+			color_code = fifo_data[76:53];
 		end
 		else //fill type is texture
 		begin
-			fill_type = fifo_data[70];
-			texture_code = fifo_data[72:71];
+			fill_type = fifo_data[52];
+			texture_code = fifo_data[78:77];
 		end
-		layer_num = fifo_data[96:94];
-		alpha_val = fifo_data[100:97];
+		alpha_val = fifo_data[82:79];
 	end
 	else
 	begin
 		alpha_val = fifo_data[3:1];
-		coordinates[63:0] = '0;
+		coordinates[47:0] = '0;
 		texture_code[1:0] = '0;
 		color_code[23:0] = '0;
 		layer_num[1:0] = '0;
