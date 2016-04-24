fcd299929e5e40d4f46f63fef1c8dd965225f8eb
yudi3160 <yudiyudidi@gmail.com>
2016-04-24 02:21:27 -0400

    fix some problem

--- fill_block/fill_block.sv
+++ fill_block/fill_block.sv
@@ -48,10 +48,10 @@ module fill_block
 	output logic write_enable,
 	input logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] read_data,
 	output logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] write_data,
-	output logic [(ADDR_SIZE_BITS-1):0] address,
+	output logic [(ADDR_SIZE_BITS-1):0] address
 
 );
-    reg [31:0] i;
+    reg [31:0] i,nexti,j;
     
     reg [7:0]xmin,nextxmin;
     reg [7:0]ymin,nextymin;
@@ -101,6 +101,7 @@ module fill_block
         end
 
     end
+    
 	reg [31:0]currentaddress, nextaddress;
 	reg [63:0]lineline;
 	reg [5:0]adr1;
@@ -112,35 +113,37 @@ module fill_block
   		if( i == 0)
   		begin
 		    if(layer_num == 0)
-		        currentaddress = 32'b0 + ymin * 256 * 24 + xmin * 24;
-
+		        currentaddress = 32'd0 + ymin * 256 * 24 + xmin * 24;
 		    else
 		        currentaddress = 32'd65536 + ymin * 256 * 24 + xmin * 24;
 		end
 		else
-		begin
-			currentaddress = nextaddress.
-		end
+			currentaddress = nextaddress;
 		
    end
-
+	
    //get sdram address for the xmin,ymin for layer buffer
+   
     always_comb
     begin
     	nextaddress = currentaddress;
     	address = '0;
     	read_enable = 0;
 		if(row_start)
+		begin
 			address = currentaddress;
 			read_enable = 1'b1;
-		
+		end
 		write_enable = 0;
 		found_flag = 0;
+		
 		if(fill_start)
+		begin
 			write_data = read_data;
         	//load 64 * 8 * 3 bits from sram
             lineline = line_buffer[i*64+:64];
         	//find a1, find a2
+        	
             for (j = 0; j < 64; j++)
             begin
                 if(lineline[j] == 1'b1)
@@ -150,9 +153,11 @@ module fill_block
                     break;
                 end             
             end
+            
             //if there is information on the line, do next step
-            if (found_flag = 1) begin
-		        for (j = 63; j >= 0; j--)
+            if (found_flag == 1) begin
+            	
+		        for (j = 63; j > 0; j--)
 		        begin
 		            if(lineline[j] == 1'b1)
 		            begin
@@ -160,29 +165,42 @@ module fill_block
 		                break;
 		            end    
 		        end
+		        
 		        //if two address are equal, only fill that pixel
+		        
 		        if (adr1 == adr2) begin
-		        	writedata[(adr1 * WORD_SIZE_BYTES * 3)+:24] = color_code;
-		       	end
-		       	else
+		        
+		        	write_data[adr1 * 24+:24] = color_code;
+		       	end else 
 		       	begin//fill anything in between
 		       		for(j = 0; j < 64; j++)
 		       		begin
+		       		
 		       			if(j >= adr1)
 		       			begin
- 							if(j > adr2)
-		       				    break
-		       				writedata[(j * WORD_SIZE_BYTES * 3)+:24] = color_code;  
-		       				    
+		       			
+ 							if(j > adr2) begin
+		       				    break;
+		       				end else begin
+		       					write_data[(j * WORD_SIZE_BYTES * 3)+:24] = color_code;  
+		       				end    
 		       			end
+		       			
+		       		end
 		       	end
+		       	
+		       	
 		    end
+		    
 		    address = currentaddress;
 		    write_enable = 1;	    
-		    nextaddress = currentaddress + 256 * 24;
+		    nextaddress = currentaddress + 8'd256 * 24;
 		    nexti = i + 1;
 		    fill_done = 1;
+		    
         end
+        
 	end
 	assign all_finish = (i >= 64);
+	
 endmodule
