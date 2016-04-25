0f8ca997034670eb495967804b6187538ae11c6c
rastogishubham <rastogis@purdue.edu>
2016-04-25 18:26:54 -0400

    changes for reset buffer

--- bresenham/tb_bresenham.sv
+++ bresenham/tb_bresenham.sv
@@ -21,13 +21,17 @@ module tb_bresenham();
 	logic [7:0]tb_x1;
 	logic [7:0]tb_y1;
 	logic tb_start;
+	logic tb_reset_buff;
 	//OUTPUTS
 	logic tb_x;	
 	logic tb_y;
 	logic [4095:0] tb_line_buffer;
 	logic [63:0][63:0] tb_picture;
 	logic tb_done;
-	
+	reg [7:0] x0;
+	reg [7:0] y0;
+	reg [7:0] x1;
+	reg [7:0] y1;
 
 	bresenham BH
 	(
@@ -40,7 +44,7 @@ module tb_bresenham();
 		.start(tb_start),
 		.x(tb_x),
 		.y(tb_y), 
-		.test(),
+		.reset_buff(tb_reset_buff),
 		.line_buffer(tb_line_buffer), 
 		.picture(tb_picture),
 		.done(tb_done) 
@@ -55,37 +59,23 @@ module tb_bresenham();
 	integer i;
 	initial
 	begin
-		tb_n_rst = 0 ;
-		tb_y0 = 8'd0; 
-		tb_x0 = 8'd0; 
-		tb_y1 = 8'd63; 
-		tb_x1 = 8'd63;  
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		tb_n_rst = 1;  
-		tb_start = 1; 
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-		#(CLK_PERIOD)
-
+		tb_n_rst = 1'b0;
+		@(negedge tb_clk);
+		tb_n_rst = 1'b1;
+		@(negedge tb_clk);
+		tb_start = 1'b1;
+		tb_reset_buff = 1'b1;
+		x0 = 8'd0;
+		y0 = 8'd0;
+		x1 = 8'd23;
+		y1 = 8'd23;
+		tb_x0 = x0;
+		tb_x1 = x1;
+		tb_y0 = y0;
+		tb_y1 = y1;
+		@(negedge tb_clk);
+		tb_reset_buff = 1'b0;
+		@(negedge tb_clk);
 		i = 1'b1;
 		
 		
