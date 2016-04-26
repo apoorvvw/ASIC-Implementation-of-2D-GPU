b7d017c67292df0f41503203a3e7ea1a5286da9a
yudi3160 <yudiyudidi@gmail.com>
2016-04-25 23:03:58 -0400

    Update fill_bla_wrapper.sv

--- fill_bla_wrapper.sv
+++ fill_bla_wrapper.sv
@@ -7,30 +7,36 @@
 // Description: Wrapper for BLA and the fill block
 
 module fill_bla_wrapper
+#(
+	ADDR_SIZE_BITS = 24,
+	WORD_SIZE_BYTES = 3,
+	DATA_SIZE_WORDS = 64
+	
+)
 (
 	input logic clk,
 	input logic n_rst,
-	input logic vertice_num,
-	input logic [47:0]coordinates,
-	input wire inst_type,
-	input wire alpha_done,
-	input wire fifo_empty,
-	input wire config_in,
-	input wire config_done,
-	input wire fill_type,
-	input wire [1:0] texture_code,
-	input wire [23:0] color_code,
-	input wire layer_num,
-	input wire [23:0] init_file_number,
-	input wire [23:0] dump_file_number,
-	input wire mem_clr,
-	input wire mem_init,
-	input wire mem_dump,
-	input wire [23:0] start_address,
-	input wire [23:0] last_address,
-	input wire verbose,
-	output reg done,
-	output reg bla_done
+
+	input logic vertice_num, //bw
+	input logic [47:0]coordinates, //bw,fill
+	input wire inst_type, //mc
+	input wire alpha_done, //mc
+	input wire fifo_empty, //mc
+	input wire config_in, //mc
+	input wire config_done, //mc
+	input wire fill_type, //fill
+	input wire [1:0] texture_code, //fill
+	input wire [23:0] color_code, //fill
+	input wire layer_num, //fill
+	
+	output logic read_enable,
+	output logic write_enable,
+	input logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] read_data,
+	output logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] write_data,
+	output reg [(ADDR_SIZE_BITS-1):0] address,
+	output reg fill_done,
+	output reg bla_done,
+	output reg [4095:0] line_buffer
 	
 );
 
@@ -39,59 +45,56 @@ module fill_bla_wrapper
 	reg bla_en;
 	reg config_en;
 	reg fill_en;
-	reg fill_done;
-	reg [4095:0] line_buffer;
 
-bla_wrapper bla_w
-(
-	.clk(clk),
-	.n_rst(n_rst),
-	.vertice_num(vertice_num),
-	.bla_en(bla_en),
-	.coordinates(coordinates),
-	.line_buffer(line_buffer),
-	.bla_done(bla_done)
-);
 
-main_controller MC
-(
-	.clk(clk),
-	.n_rst(n_rst),
-	.inst_type(inst_type),
-	.alpha_done(alpha_done),
-	.fifo_empty(fifo_empty),
-	.bla_done(bla_done),
-	.config_in(config_in),
-	.config_done(config_done),
-	.fill_done(fill_done),
-	.read_en(read_en),
-	.alpha_en(alpha_en),
-	.bla_en(bla_en),
-	.config_en(config_en),
-	.fill_en(fill_en)
-);
+	bla_wrapper bla_w
+	(
+		.clk(clk),
+		.n_rst(n_rst),
+		.vertice_num(vertice_num), //come from outside
+		.bla_en(bla_en), //come from mc
+		.coordinates(coordinates), //come from outside
+		.line_buffer(line_buffer), //goes to fw
+		.bla_done(bla_done) //goes to outside
+	);
 
-fill_wrapper FILL_W
-(
-	.clk(clk),
-	.n_rst(n_rst),
-	.fill_en(fill_en),
-	.done(done),
-	.fill_type(fill_type),
-	.coordinates(coordinates),
-	.texture_code(texture_code),
-	.color_code(color_code),
-	.layer_num(layer_num),
-	.line_buffer(line_buffer),
-	.init_file_number(init_file_number),
-	.dump_file_number(dump_file_number),
-	.mem_clr(mem_clr),
-	.mem_init(mem_init),
-	.mem_dump(mem_dump),
-	.start_address(start_address),
-	.last_address(last_address),
-	.verbose(verbose)
+	main_controller MC
+	(
+		.clk(clk),
+		.n_rst(n_rst),
+		.inst_type(inst_type), //come from outside
+		.alpha_done(alpha_done), //come from outside
+		.fifo_empty(fifo_empty), //come from outside
+		.bla_done(bla_done), //come from bw
+		.config_in(config_in), //come from conf
+		.config_done(config_done), //come from conf
+		.fill_done(fill_done), //come from fw
+		.read_en(read_en), //goes to FIFO
+		.alpha_en(alpha_en), //goes to alp
+		.bla_en(bla_en), //goes to bw
+		.config_en(config_en), //goes to conf
+		.fill_en(fill_en) //goes to fill
+	); 
+
+	fill_wrapper FW
+	(
+		.clk(clk),
+		.n_rst(n_rst),
+		.fill_en(fill_en),//come from mc
+		.done(fill_done), //goes to mc
+		.fill_type(fill_type), //come from outside
+		
+		.coordinates(coordinates), //come from outside
+		.texture_code(texture_code), //come from outside
+		.color_code(color_code), //come from outside
+		.layer_num(layer_num), //come from outside
+		.line_buffer(line_buffer), //come from outside
 	
-);
+		.read_enable(read_enable), //goes to outside
+		.write_enable(write_enable), //goes to outside
+		.address(address), //goes to outside
+		.read_data(read_data), //come from outside
+		.write_data(write_data) //goes to outside
+	);
 
 endmodule
