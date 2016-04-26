fee3fe4ede895a17fb6cb79b6ee9cecc9c9fbf0f
yudi3160 <yudiyudidi@gmail.com>
2016-04-26 19:56:46 -0400

    Update fill_wrapper.sv

--- fill_block/fill_wrapper.sv
+++ fill_block/fill_wrapper.sv
@@ -27,24 +27,15 @@ module fill_wrapper
 	input logic [23:0] color_code,
 	input logic layer_num,
 	input logic [4095:0] line_buffer,
+	
+	output logic read_enable,
+	output logic write_enable,
+	input logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] read_data,
+	output logic [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] write_data,
+	output reg [(ADDR_SIZE_BITS-1):0] address
 
-	
-	input logic [(ADDR_SIZE_BITS-1):0] init_file_number,
-	input logic [(ADDR_SIZE_BITS-1):0] dump_file_number,
-	input wire mem_clr,
-	input wire mem_init,
-	input wire mem_dump,
-	input logic [(ADDR_SIZE_BITS-1):0] start_address,
-	input logic [(ADDR_SIZE_BITS-1):0] last_address,
-	input wire verbose
-	
 );
 
-	reg read_enable;
-	reg write_enable;
-	reg [(ADDR_SIZE_BITS-1):0] address;
-	reg [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] write_data;
-	reg [((WORD_SIZE_BYTES * DATA_SIZE_WORDS * 8) - 1):0] read_data;
 	
 	reg math_start;
 	reg row_start;
@@ -91,26 +82,6 @@ module fill_wrapper
 
 	);
 	
-	on_chip_sram_wrapper SRAM
-	(
-		.init_file_number(init_file_number),
-		.dump_file_number(dump_file_number),
-		.mem_clr(mem_clr),
-		.mem_init(mem_init),
-		.mem_dump(mem_dump),
-		.start_address(start_address),
-		.last_address(last_address),
-		.verbose(verbose),
-		.read_enable(read_enable),
-		.write_enable(write_enable),
-		.address(address),
-		.read_data(read_data),
-		.write_data(write_data)
-
-	);
-
-
-
 
 
 
