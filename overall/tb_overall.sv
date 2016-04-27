cc890a409962e671c1b28c48b8b9460ad669acc4
rastogishubham <rastogis@purdue.edu>
2016-04-27 14:56:22 -0400

    fixed 90 ns problem

--- overall/tb_overall.sv
+++ overall/tb_overall.sv
@@ -165,6 +165,15 @@ module tb_overall();
 		#TB_CLK_PERIOD;
 		while(1 == 1)
 		begin
+			#TB_CLK_PERIOD;
+			if(tb_bla_done == 1'b1)
+				break;
+				
+		end
+		#TB_CLK_PERIOD;
+		while(1 == 1)
+		begin
+			#TB_CLK_PERIOD;
 			if(tb_fill_done == 1'b1)
 				break;
 		end
@@ -186,8 +195,19 @@ module tb_overall();
 		tb_fifo_data = {alpha_val, texture_code, color_code, fill_type, layer_num, y2, x2, y1, x1, y0, x0, vertice_num, inst_type};
 		#TB_CLK_PERIOD;
 		tb_fifo_empty = 1'b1;
+		#TB_CLK_PERIOD;
+		#TB_CLK_PERIOD;
 		while(1 == 1)
 		begin
+			#TB_CLK_PERIOD;
+			if(tb_bla_done == 1'b1)
+				break;
+				
+		end
+		#TB_CLK_PERIOD;
+		while(1 == 1)
+		begin
+			#TB_CLK_PERIOD;
 			if(tb_fill_done == 1'b1)
 				break;
 		end
@@ -207,8 +227,10 @@ module tb_overall();
 		inst_type = 1'b1;
 		fill_type = 1'b0;
 		tb_fifo_data = {alpha_val, texture_code, color_code, fill_type, layer_num, y2, x2, y1, x1, y0, x0, vertice_num, inst_type};
+		#TB_CLK_PERIOD;			
 		while(1 == 1)	
 		begin
+			#TB_CLK_PERIOD;
 			if(tb_alpha_done)
 				break;
 		end
