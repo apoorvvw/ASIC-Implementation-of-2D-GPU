ca2eeb00673f01899251a454d6315ba77cd92eb3
yudi3160 <yudiyudidi@gmail.com>
2016-04-26 21:50:41 -0400

    This py draw 3 buffers on SDRAM to pictrues

--- draw.py
+++ draw.py
@@ -7,15 +7,32 @@ if __name__ == "__main__":
 		alllines = fptr.readlines()
 	j = 0
 	k = 0
-	img = np.ndarray(shape=(64,64,3),dtype=np.uint8)
+	buffer1 = np.ndarray(shape=(256,256,3),dtype=np.uint8)
+	buffer2 = np.ndarray(shape=(256,256,3),dtype=np.uint8)
+	buffer3 = np.ndarray(shape=(256,256,3),dtype=np.uint8)
 	for i in range(1, len(alllines)):
-		m = re.search(r"\:(.*)\;",alllines[i])
-		img[j][k][0] = int(m.group(1)[0:2],16)
-		img[j][k][1] = int(m.group(1)[2:4],16)	
-		img[j][k][2] = int(m.group(1)[4:6],16)
-		k += 1
-		if k == 64 :
-			j += 1
-			k = 0
-	imsave("shu.png",img)
+		m = re.search(r"(.*)\:(.*)\;",alllines[i])
+		address = int(m.group(1))
+		if 0 <= address <= 65535: 
+			j = address / 256
+			k = address % 256
+			buffer1[j][k][0] = int(m.group(2)[0:2],16)
+			buffer1[j][k][1] = int(m.group(2)[2:4],16)	
+			buffer1[j][k][2] = int(m.group(2)[4:6],16)
+		elif 65536 <= address <= 131071:			
+			j = (address - 63356) / 256
+			k = (address - 63356) % 256
+			buffer2[j][k][0] = int(m.group(2)[0:2],16)
+			buffer2[j][k][1] = int(m.group(2)[2:4],16)	
+			buffer2[j][k][2] = int(m.group(2)[4:6],16)		
+			
+		elif 143360  <= address <= 208895:
+			j = (address - 144360) / 256
+			k = (address - 144360) % 256
+			buffer3[j][k][0] = int(m.group(2)[0:2],16)
+			buffer3[j][k][1] = int(m.group(2)[2:4],16)	
+			buffer3[j][k][2] = int(m.group(2)[4:6],16)
+	imsave("buffer1.png",buffer1)
+	imsave("buffer2.png",buffer2)
+	imsave("outputbuffer.png",buffer3)
 		
