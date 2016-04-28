161b9ffd32a58d7161ece49b5a501fcd369d8ecd
Apoorv Wairagade <awairaga@purdue.edu>
2016-04-28 13:25:53 -0400

    Update sram.sv
    Final

--- SRAM/sram.sv
+++ SRAM/sram.sv
@@ -1,13 +1,30 @@
-module single_clk_ram(
- output reg [7:0] q,
- input [7:0] d,
- input [6:0] write_address, read_address,
- input we, clk
+// $Id: $
+// File name:   sram.sv
+// Created:     5/24/2016
+// Author:      Apoorv Vijay Wairagade
+// Lab Section: 5
+// Version:     1.0  Initial Design Entry
+// Description: A simple on-chip sram.
+ 
+
+module sram (
+	
+	output reg [(24 - 1):0] read_data,
+
+	input [(24 - 1):0] write_data,
+	input [(24 - 1):0] address,
+	input write_enable,
+	input read_enable,
+	input clk
 );
- reg [7:0] mem [127:0];
- always @ (posedge clk) begin
- if (we)
- mem[write_address] <= d;
- q <= mem[read_address]; // q doesn't get d in this clock cycle
- end
+	reg [23:0] mem [127:0];
+	
+	always @ (posedge clk) begin
+		if (write_enable)
+			mem[address] <= write_data;
+
+		if (read_enable)
+			read_data <= mem[address]; // q doesn't get d in this clock cycle
+	end
+
 endmodule
