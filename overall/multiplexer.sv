024f5736dde649e61ba1fa8453acf7fdd5782991
pkale <pkale@purdue.edu>
2016-05-04 23:43:51 -0400

    Added comments / Final commit

--- overall/multiplexer.sv
+++ overall/multiplexer.sv
@@ -8,44 +8,37 @@
 
 module multiplexer
 (
-	input logic alpha_en,
-	input logic fill_en,
+	input logic alpha_en, // This signal selects which block controls the SRAM 
 	
-	input logic f_read_enable,
-	input logic f_write_enable,
-	input logic [23:0] f_address,
-	input logic [1535:0]f_write_data,
+	input wire read_enable1,
+	input wire write_enable1,
+	input wire [18:0] address1,
+	input wire [1535:0]write_data1,
 	
-	input logic a_read_enable,
-	input logic a_write_enable,
-	input logic [23:0] a_address,
-	input logic [1535:0]a_write_data,
+	input wire read_enable2,
+	input wire write_enable2,
+	input wire [18:0] address2,
+	input wire [1535:0]write_data2,
 		
-	output logic read_enable,
-	output logic [23:0]address,
-	output logic [1535:0]write_data,
-	output logic write_enable
+	output wire read_enable,
+	output wire [18:0]address,
+	output wire [1535:0]write_data,
+	output wire write_enable
 
 );
-	always_comb
-	begin
-		read_enable = 0;
-		write_enable = 0;
-		address = '0;
-		write_data = '0;
-		if(alpha_en) begin
-			read_enable = a_read_enable;
-			write_enable = a_write_enable;
-			address = a_address;
-			write_data = a_write_data;
-		end else if (fill_en) begin
-			read_enable = f_read_enable;
-			write_enable = f_write_enable;
-			address = f_address;
-			write_data = f_write_data;	
-		end
-	
-	end
+
+	/*
+		
+		The deisgn uses Multiplexers to make sure that SRAM gets only one set 
+		of enabale signals so same signal does not conflict
+
+		This Multiplexer seperates Fill and alpha signal
+	*/
+
+	assign read_enable = (alpha_en) ? read_enable1 : read_enable2;
+	assign write_enable = (alpha_en) ? write_enable1 : write_enable2;
+	assign address = (alpha_en) ? address1 : address2;
+	assign write_data = (alpha_en) ? write_data1 : write_data2;
 
 
 
