f5dab2c19c99caf487b1e1bacdb6cb77377dd35a
pkale <pkale@purdue.edu>
2016-05-05 00:07:56 -0400

    Added comments / Final commit
    Also reanmed

--- overall/fifo.sv
+++ overall/fifo.sv
@@ -4,27 +4,35 @@
 // Author:      Yudi Wu
 // Lab Section: 337-04
 // Version:     1.0  Initial Design Entry
-// Description: FIFO
-
-module shubham //FIFO
+// Description: 82 bit wide FIFO
+module fifo //FIFO
 (
     input wire clk,
     input wire n_rst,
     input wire r_enable,
     input wire w_enable,
-    input wire [82:0]w_data,
-    output reg [82:0]r_data,
+    input wire [81:0]w_data, // This is 82 bits wide since all the input data concatinated togeather is 82 bits long
+    
+    output reg [81:0]r_data,
     output reg empty,
     output reg full
 
 );
-    reg [82:0]data1, next_data1;
-    reg [82:0]data2, next_data2;    
-    reg [82:0]data3, next_data3;
-    reg [82:0]data4, next_data4;
-    reg [2:0] w_pointer, next_w_pointer;
-	reg [82:0] nextr_data;
+
+  /*
+    This FIFO uses the concept of circular buffers from Lab 6 of ECE 337 at Purdue University
+  */
+
+  // VARIABLE DECLARATION
+  reg [81:0]data1, next_data1;
+  reg [81:0]data2, next_data2;    
+  reg [81:0]data3, next_data3;
+  reg [81:0]data4, next_data4;
+  reg [2:0] w_pointer, next_w_pointer;
+	reg [81:0] nextr_data;
 	reg next_empty, next_full;
+
+  // STATE REGISTER
   always_ff @ (posedge clk, negedge n_rst)
   begin
     if(n_rst == 0)
@@ -40,7 +48,7 @@ module shubham //FIFO
     end
     else
       begin
-	 	 data1 = next_data1;
+	     	 data1 = next_data1;
          data2 = next_data2;
          data3 = next_data3; 
          data4 = next_data4;
@@ -54,7 +62,7 @@ module shubham //FIFO
   end
 
 
-  
+  // NEXT STATE LOGIC and OUTPUT LOGIC
   always_comb begin //load_info
       next_data1 = data1;
       next_data2 = data2;
@@ -62,6 +70,7 @@ module shubham //FIFO
       next_data4 = data4;
       nextr_data = r_data;
       next_w_pointer = w_pointer;
+      
       if(w_enable) begin
       	 if(~full) begin
       	       next_w_pointer = w_pointer + 1;    
@@ -84,11 +93,11 @@ module shubham //FIFO
       else if(r_enable) begin
       		if(~empty) begin
       		    next_w_pointer = w_pointer - 1; 
-      			nextr_data = data1;
-		  		next_data1 = data2;
-		  		next_data2 = data3;
-		  		next_data3 = data4;
-		  		next_data4 = '0;
+        			nextr_data = data1;
+          		next_data1 = data2;
+          		next_data2 = data3;
+          		next_data3 = data4;
+          		next_data4 = '0;
 		  	end
 
       end
