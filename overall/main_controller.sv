269f03cccd2db59a4e9ccc922508339ca6ba1d44
rastogishubham <rastogis@purdue.edu>
2016-05-04 23:47:22 -0400

    commented

--- overall/main_controller.sv
+++ overall/main_controller.sv
@@ -8,20 +8,20 @@
 
 module main_controller
 (
-	input wire clk,
-	input wire n_rst,
-	input wire inst_type,
-	input wire alpha_done,
-	input wire fifo_empty,
-	input wire bla_done,
-	input wire config_in,
-	input wire config_done,
-	input wire fill_done,
-	output reg read_en,
-	output reg alpha_en,
-	output reg bla_en,
-	output reg config_en,
-	output reg fill_en
+	input wire clk, //clock
+	input wire n_rst, //reset
+	input wire inst_type, // the type of instruction
+	input wire alpha_done, //alpha done from alpha block
+	input wire fifo_empty, //empty flag from fifo
+	input wire bla_done, //done flag from the BLA block
+	input wire config_in, //config in from slave
+	input wire config_done, //done flag form the config block
+	input wire fill_done, //done flag from the fill block
+	output reg read_en, //read enable to the FIFO
+	output reg alpha_en, //Alpha enablke to the alpha block
+	output reg bla_en, //Enable to the BLA block
+	output reg config_en, //Enable to the config block
+	output reg fill_en //Enable to the fill block
 );
 typedef enum logic [3:0] {IDLE, CONFIG, DECODE, BLA, FILL, ALPHA, DONE, WAIT_BLA, WAIT_FILL, WAIT_ALPHA, WAIT_CONFIG}
 	state_type;
@@ -29,12 +29,13 @@ typedef enum logic [3:0] {IDLE, CONFIG, DECODE, BLA, FILL, ALPHA, DONE, WAIT_BLA
 	always_ff @ (posedge clk, negedge n_rst)
 	begin
 		if(n_rst == 1'b0)
-			state <= IDLE;
+			state <= IDLE; //reset to idle state
 		else
-			state <= next_state;
+			state <= next_state; //next state logic
 	end
 	always_comb
 	begin
+		//initial declarations to avoid latches
 		next_state = state;
 		read_en = 1'b0;
 		alpha_en = 1'b0;
@@ -42,202 +43,90 @@ typedef enum logic [3:0] {IDLE, CONFIG, DECODE, BLA, FILL, ALPHA, DONE, WAIT_BLA
 		config_en = 1'b0;
 		fill_en = 1'b0;
 		case(state)
+		
+		//Idle state 
 		IDLE: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			//If config in is high go to config block
 			if(config_in == 1'b1)
 				next_state = CONFIG;
+			//else stay in idle
 			else
 				next_state = IDLE;
 		end
+		//Config state
 		CONFIG: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b1;
-			fill_en = 1'b0;
+			config_en = 1'b1; //config enable high
+			//If config done is high move to wait state
 			if(config_done == 1'b1)
 				next_state = WAIT_CONFIG;
+			//else stay in config state
 			else
 				next_state = CONFIG;	
 		end
+		// wait state for 1 clock cycle
 		WAIT_CONFIG: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			//Go to decode after a clock cycle
 			next_state = DECODE;
 		end
+		//decode state
 		DECODE: begin
-			read_en = 1'b1;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
-			if(inst_type == 1'b0) //instruction type is a shape
+			read_en = 1'b1; //read enable high to fifo
+			//instruction type is a shape
+			if(inst_type == 1'b0) 
 				next_state = BLA;
+			//instruction type is alpha
 			else
 				next_state = ALPHA;
 		end
+		//Bresenham state
 		BLA: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b1;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			bla_en = 1'b1; //BLA enable high, start drawing on line buffer
+			//BLA done then got to wait state
 			if(bla_done == 1'b1)
 				next_state = WAIT_BLA;
+			//else stay in BLA state
 			else
 				next_state = BLA;
 		end
+		// wait state for 1 clock cycle
 		WAIT_BLA: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
-			next_state = FILL;		
+			next_state = FILL;	//go to fill state after one clock cycle	
 		end
+		//Fill state
 		FILL: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b1;
+			fill_en = 1'b1; //fill enable high, start filling on SRAM
+			//if fill done, go to wait state
 			if(fill_done == 1'b1)
 				next_state = WAIT_FILL;
+			//else stay in fill state
 			else
 				next_state = FILL;		
 		end
+		//wait state till fifo has more instructions
 		WAIT_FILL: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			//stay till fifo is not empty
 			if(fifo_empty == 1'b1)
 				next_state = WAIT_FILL;
 			else
 				next_state = DECODE;
 		
 		end
+		//Alpha state
 		ALPHA: begin
-			read_en = 1'b0;
-			alpha_en = 1'b1;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			alpha_en = 1'b1; //Alpha enable high, start alpha blend
+			//If alpha done, go to wait state
 			if(alpha_done == 1'b1)
 				next_state = WAIT_ALPHA;
+			//else stay in alpha state
 			else
 				next_state = ALPHA;
 		end
+		//Wait state for 1 clock cycle
 		WAIT_ALPHA: begin
-			read_en = 1'b0;
-			alpha_en = 1'b0;
-			bla_en = 1'b0;
-			config_en = 1'b0;
-			fill_en = 1'b0;
+			//go to IDLE after 1 clock cycle
 			next_state = IDLE;
 		end
 		endcase
 		
 	end
 endmodule
-/*
-typedef enum logic [3:0] {IDLE, CONFIG, DECODE, DEC&DRAW, DRAW, ALPHA} 
-	state_type state, next_state;
-	always_ff @ (posedge clk, negedge n_rst)
-	begin
-		if(n_rst == 0)
-			state <= IDLE;     
-		else
-			state <= next_state;	    
-	end
-	always_comb
-	begin
-		next_state = state;
-		case (state)
-		IDLE: begin
-			config_en = 1'b0;
-			read_en = 1'b0;
-			draw_en = 1'b0;
-			alpha_en = 1'b0;
-			if(config_in == 1'b1)
-				next_state = CONFIG;
-			else
-				next_state = IDLE;
-		end
-		CONFIG: begin
-			config_en = 1'b1;
-			read_en = 1'b0;
-			draw_en = 1'b0;
-			alpha_en = 1'b0;
-			if(config_done == 1'b1 && fifo_empty == 1'b0)
-				next_state = DECODE;
-			else
-				next_state = CONFIG;
-		end
-		DECODE: begin
-			config_en = 1'b0;
-			read_en = 1'b1;
-			draw_en = 1'b0;
-			alpha_en = 1'b0;
-			if(decode_fin == 1'b1 && fifo_empty == 1'b0)
-				next_state = DEC&DRAW;
-			else if(decode_fin == 1'b1 && fifo_empty == 1'b1)
-				next_state = DRAW;
-			else if(inst_type == 1'b1) //Alpha instruction
-				next_state = ALPHA;
-		end
-		DEC&DRAW: begin
-			config_en = 1'b0;
-			read_en = 1'b1;
-			draw_en = 1'b1;
-			alpha_en = 1'b0;
-			if(decode_fin == 1'b1 && draw_fin == 1'b0)
-				next_state = DRAW;
-			//else if(decode_fin == 1'b0 && draw_fin == 1'b1 && decode_full == 1'b0) //if decode is done before draw then just make draw enable low because the first decode state is used when 2 instructions have been drawn and filled
-			//draw_en = 1'b0;
-			else if(decode_fin == 1'b0 && draw_fin == 1'b1)
-			begin
-				next_state = DECODE;
-			end
-			else
-				next_state = DEC&DRAW;
-			
-		end
-		DRAW: begin
-			config_en = 1'b0;
-			read_en = 1'b0;
-			draw_en = 1'b1;
-			alpha_en = 1'b0;
-			if(inst_type == 1'b1 && draw_fin == 1'b1)
-				next_state = ALPHA;
-			//else if(inst_type == 1'b1 && draw_fin == 1'b0)
-			//	next_state = DRAW;
-			//redundant because if alpha is there or not only oscillate between dec&draw and draw depending on the cases
-			else if(fifo_empty == 1'b0 && decode_full == 1'b0 && draw_fin == 1'b1)
-				next_state = DECODE;
-			else if(fifo_empty == 1'b0 && decode_full == 1'b0 && draw_fin == 1'b0)
-				next_state = DEC&DRAW;
-			else if(fifo_empty == 1'b0 && decode_full == 1'b1 && draw_fin == 1'b0)
-				next_state = DRAW;
-		end
-		ALPHA: begin
-			config_en = 1'b0;
-			read_en = 1'b0;
-			draw_en = 1'b0;
-			alpha_en = 1'b1;
-			if(alpha_fin == 1'b0)
-				next_state = ALPHA
-			else
-				next_state = IDLE;
-		end
-		endcase
-	end*/
-			
